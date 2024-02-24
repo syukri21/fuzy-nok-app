61249ec453580b38596c55b484e8caec0e1d1565
syukri21 <syukrihsb148@gmail.com>
2024-02-24 23:28:34 +0700

    feat: add vendor

---
+++ app/Entities/UserEntity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class UserEntity extends Entity
+{
+    protected string $table = 'users';
+    protected $datamap = [];
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
+    protected $casts   = [];
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\Entities\UserEntity;
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'username',
+        'nik',
+        'password',
+        'verify_at'
+    ];
+    protected $useAutoIncrement = true;
+    protected $returnType = UserEntity::class;
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
