3e72bc0a7d4bb9fe71954de43f6a48179ce24ff8
syukri21 <syukrihsb148@gmail.com>
2024-03-01 23:10:58 +0700

    fix layout

---
+++ app/Views/Layout/AdminLayout/Footer.php
@@ -0,0 +1,20 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url() ?>dependencies/jquery/jquery.min.js"></script>
+<script src="<?= base_url() ?>dependencies/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url() ?>dependencies/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url() ?>js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="<?= base_url() ?>dependencies/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url() ?>dependencies/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="<?= base_url() ?>js/demo/datatables-demo.js"></script>
+
+</body>
+
+</html>
--- app/Views/Login/Login.php
+++ app/Views/Login/Login.php
@@ -23,14 +23,10 @@
 </head>
 
 <body class="bg-gradient-primary">
-
 <div class="container">
-
     <!-- Outer Row -->
     <div class="row justify-content-center">
-
         <div class="col-xl-10 col-lg-12 col-md-9">
-
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
@@ -78,6 +74,7 @@
             </div>
         </div>
     </div>
+</div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="<?= base_url() ?>dependencies/jquery/jquery.min.js"></script>
@@ -88,7 +85,6 @@
 
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url() ?>js/sb-admin-2.min.js"></script>
-
 </body>
 
 </html>
