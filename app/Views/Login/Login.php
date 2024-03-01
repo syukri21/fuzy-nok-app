e9c94e31aa4752762baf3c31df4a92f0319f802b
syukri21 <syukrihsb148@gmail.com>
2024-03-01 22:51:05 +0700

    fix username password salah

--- app/Views/Login/Login.php
+++ app/Views/Login/Login.php
@@ -40,6 +40,7 @@
                             <div class="p-5">
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    <p class="alert-danger rounded"><?= session()->getFlashdata('error') ?></p>
                                 </div>
                                 <form class="user" action="<?= base_url() ?>login" method="post">
                                     <div class="form-group">
